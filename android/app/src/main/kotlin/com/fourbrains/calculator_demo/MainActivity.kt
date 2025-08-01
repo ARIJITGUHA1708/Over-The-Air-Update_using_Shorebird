84fe4724f4a469bfedb4632fd0f84eba41693ed0
Arijit Guha <arijitguha@Arijits-MacBook-Pro.local>
2025-08-01 23:28:51 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/fourbrains/calculator_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fourbrains.calculator_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Pages/HomePage.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart';
+import 'package:toastification/toastification.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final updater = ShorebirdUpdater();
+  int? _currentPatch;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Get the current patch number and print it to the console.
+    // It will be `null` if no patches are installed.
+    updater
+        .readCurrentPatch()
+        .then((currentPatch) {
+          print('The current patch number is: ${currentPatch?.number}');
+          setState(() {
+            _currentPatch = currentPatch?.number;
+          });
+        })
+        .then((value) => _checkForUpdates());
+  }
+
+  Future<void> _checkForUpdates() async {
+    // Check whether a new update is available.
+    final status = await updater.checkForUpdate();
+
+    if (status == UpdateStatus.outdated) {
+      try {
+        ScaffoldMessenger.of(context).showMaterialBanner(
+          MaterialBanner(
+            content: Text("'New Update Available"),
+            actions: [Text("Dismiss")],
+          ),
+        );
+        toastification.show(
+          autoCloseDuration: Duration(seconds: 4),
+          type: ToastificationType.success,
+          style: ToastificationStyle.flat,
+          title: Text('New Update Available'),
+        );
+        // Perform the update
+        await updater.update(track: UpdateTrack.stable);
+      } on UpdateException catch (error) {
+        // Handle any errors that occur while updating.
+      }
+    } else if (status == UpdateStatus.upToDate) {
+      try {
+        ScaffoldMessenger.of(context).showMaterialBanner(
+          MaterialBanner(
+            content: Text("No Available update"),
+            actions: [Text("Dismiss")],
+          ),
+        );
+        toastification.show(
+          autoCloseDuration: Duration(seconds: 4),
+
+          type: ToastificationType.success,
+          style: ToastificationStyle.flat,
+
+          title: Text('No update available'),
+        );
+      } catch (error) {
+        toastification.show(
+          autoCloseDuration: Duration(seconds: 4),
+          type: ToastificationType.error,
+          style: ToastificationStyle.flat,
+          title: Text('$error'),
+        );
+      }
+    } else if (status == UpdateStatus.restartRequired) {
+      try {
+        ScaffoldMessenger.of(context).showMaterialBanner(
+          MaterialBanner(
+            content: Text("Restart Your App"),
+            actions: [Text("Dismiss")],
+          ),
+        );
+        toastification.show(
+          autoCloseDuration: Duration(seconds: 4),
+          type: ToastificationType.success,
+          style: ToastificationStyle.flat,
+          title: Text('Restart Your App'),
+        );
+      } catch (error) {
+        toastification.show(
+          autoCloseDuration: Duration(seconds: 4),
+          type: ToastificationType.error,
+          style: ToastificationStyle.flat,
+          title: Text('$error'),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: ElevatedButton(
+        onPressed: () {
+          debugPrint("-----------");
+          _checkForUpdates();
+        },
+        child: Icon(Icons.refresh),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child:
+                (_currentPatch == null)
+                    ? Text("No Patch Available")
+                    : Text("Current Patch: $_currentPatch"),
+          ),
+          Center(child: Text(" Arijit")),
+        ],
+      ),
+    );
+  }
+}
